1054cd4e189173fcfab1cac67a402232a8a955f0
Pinn-Faker <sopheakpin342@gmail.com>
2023-08-13 20:22:03 +0700

    Reinitialize

---
+++ M1site/M1site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M1site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
